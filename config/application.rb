eff81122e090de38f0b41d7386c9554dcd5dbff2
TheDevsTeam <cheikhndiongue3@gmail.com>
2018-09-29 19:02:09 -0400

    added new slidshow with images and I also added audio tag functionality

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module News
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.precompile << "audios/*"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
