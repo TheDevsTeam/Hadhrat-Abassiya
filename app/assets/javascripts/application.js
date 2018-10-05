c12933cd1adce8ba53470e65fe01ceeb9ac767cb
TheDevsTeam <cheikhndiongue3@gmail.com>
2018-10-05 15:24:57 -0400

    added more audio files and images to the home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,16 @@
 document.addEventListener("turbolinks:load", function() {
   // ...
 });
+
+
+//FEATURED HOVER
+$(document).ready(function(){
+      $(".linkfeat").hover(
+        function () {
+            $(".textfeat").show(500);
+        },
+        function () {
+            $(".textfeat").hide(500);
+        }
+    );
+});
