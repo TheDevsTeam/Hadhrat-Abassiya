bfe8dc057c5005709735d8d3ddedbf0d86a161e4
TheDevsTeam <cheikhndiongue3@gmail.com>
2018-10-21 15:49:14 -0400

    using tinymce instead of ckeditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,13 @@
 
 
 document.addEventListener("turbolinks:load", function() {
-  // ...
+	tinymce.remove();
+	tinymce.init({ 
+		selector:'textarea#post_body',
+		plugins: "codesample image media link code",
+		toolbar: "undo redo | styleselect | bold italic link| codesample | image media | code"
+	});
+
 });
 
 
